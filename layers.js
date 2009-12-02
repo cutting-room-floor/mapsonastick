d15ad3b860a3e350574e5df2d68b61b8c91b0104
Tom MacWright <macwright@gmail.com>
2009-12-02 15:17:52 -0500

    Getting up to date on other changes

--- layers.js
+++
@@ -1,9 +0,0 @@
-var layers = [
-/*  new OpenLayers.Layer.GML( 'Polling Centers', 'polling_centers.kml', 
-  {
-    format:OpenLayers.Format.KML, 
-    projection: new OpenLayers.Projection('EPSG:4326'),
-    formatOptions:{},styleMap:default_styles['medical'],
-    }
-  ) */
-]
