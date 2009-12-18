ef5c4e6f77400c9fbac9e51d958edd6445247441
Tom MacWright <macwright@gmail.com>
2009-12-18 13:45:12 -0500

    Fixes that I should port to main

--- resources/layers.js
+++ resources/layers.js
@@ -6,7 +6,34 @@
  * but for shipping default maps, this is the place to define them.
  */
 
+/**
+ * IMPORTANT NOTE:
+ *
+ * between layer definitions in the array below, there must be commas,
+ * but after the last layer definition, there should not be a comma.
+ *
+ * For example, it could look like
+ *
+ * new OpenLayers.Layer.TMS('World',
+ *    .... LAYER DEFINITION
+ * ),
+ * new OpenLayers.Layer.TMS('Mars',
+ *    .... LAYER DEFINITION
+ * )
+ *
+ * Note the comma after the World definition and the lack of comma
+ * after Mars. Incorrectly placing commas will break the site in
+ * various web browsers
+ */
+
 var layers = [
+/*
+ * To those interested in adding a new offline tileset,
+ * duplicate the chunk of code below and rename 
+ * 'World' and 'world-dark' to whatever you want your
+ * layer to be called in the legend and what the 
+ * tileset directory is called, respectively.
+ */
   new OpenLayers.Layer.TMS('World',
       'tiles/',
       {
