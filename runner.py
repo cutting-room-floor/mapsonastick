cef21d0cb3de8418282636a48c690455a8e54d88
Tom MacWright <macwright@gmail.com>
2010-07-13 16:42:42 -0400

    Finishing up changes

--- runner.py
+++ runner.py
@@ -5,7 +5,27 @@
 import sys
 from multiprocessing import Process
 from mapsonastick import server
-from PyQt4 import QtGui, QtCore
+from PyQt4 import QtGui, QtCore, _qt
+
+class DropButton(QtGui.QPushButton):
+    def __init__(self, title, parent):
+        QtGui.QPushButton.__init__(self, title, parent)
+        self.setAcceptDrops(True)
+
+    def mousePressEvent(self, event):
+        filename = QtGui.QFileDialog.getOpenFileName(self, 'Open file',
+            '')
+        print filename
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasFormat('text/uri-list'):
+            event.accept()
+        else:
+            event.ignore() 
+
+    def dropEvent(self, event):
+        print event.mimeData().text() 
+
 
 class MainWindow(QtGui.QMainWindow):
     def __init__(self):
@@ -20,11 +40,23 @@ class MainWindow(QtGui.QMainWindow):
         self.serverLink = QtGui.QPlainTextEdit()
         self.serverLink.setReadOnly(True)
         self.serverLink.appendHtml("<a href='http://localhost:5000/'>Maps on a Stick</a>")
-        self.setCentralWidget(self.serverLink)
+        # self.setCentralWidget(self.serverLink)
         
         self.exit = QtGui.QAction(QtGui.QIcon('icons/process-stop.png'), 'Exit', self)
         self.exit.setShortcut('Ctrl+Q')
         self.connect(self.exit, QtCore.SIGNAL('triggered()'), self.stopClick)
+
+        self.model = QtGui.QStringListModel() 
+        self.tableView = QtGui.QTableView()
+        self.tableView.setModel(self.model) 
+
+        self.dropButton = DropButton('Add KML', self)
+        self.dropButton.move(10, 50)
+
+        # stringList = QtCore.QStringList() 
+        # stringList << "World-Light_z0-10_v1.mbtiles"
+        # self.model.setStringList(stringList) 
+        # self.setCentralWidget(self.tableView)
 
         self.start = QtGui.QAction(QtGui.QIcon('icons/go-next.png'), 'Start', self)
         self.connect(self.start, QtCore.SIGNAL('triggered()'), self.startClick)
--- setup.py
+++ setup.py
@@ -7,5 +7,13 @@ setup(
     packages=['mapsonastick'],
     include_package_data=True,
     zip_safe=False,
-    install_requires=['Flask']
+    app=['runner.py'],
+    options={
+      'py2app': {
+        'argv_emulation': True,
+        'includes': ['PyQt4', 'PyQt4.QtCore', 'PyQt4.QtGui', 'PyQt4._qt'],
+        'excludes': ['PyQt4.QtDesigner', 'PyQt4.QtNetwork', 'PyQt4.QtOpenGL', 'PyQt4.QtScript', 'PyQt4.QtSql', 'PyQt4.QtTest', 'PyQt4.QtWebKit', 'PyQt4.QtXml', 'PyQt4.phonon']
+        }
+      },
+    install_requires=['flask', 'werkzeug']
 )
